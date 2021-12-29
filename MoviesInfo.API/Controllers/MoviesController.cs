9c205cebf9e72ee349bf3a741cbca565c2689fd0
nikita <nbron86@gmail.com>
2021-12-29 21:58:06 +0200

    Add project files.

---
+++ MoviesInfo.API/Controllers/MoviesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MoviesInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+
+    public class MoviesController:ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+      
+
+        private readonly IHttpClientFactory _clientFactory;
+        public MoviesController(IHttpClientFactory httpClientFactory,IConfiguration config)
+        {
+            _clientFactory = httpClientFactory;
+            _config = config;
+        }
+
+        [HttpGet("{Title}")]
+        public async Task<ActionResult<Movie>> Series(string title)
+        {
+            try
+            {
+                var apiKey = _config.GetValue<string>("apiKey");
+                var t = title;
+
+                var httpClient = _clientFactory.CreateClient();
+                string omdbApiUrl = $"http://www.omdbapi.com?apikey={apiKey}&t={t}";
+                var responseString = await httpClient.GetStringAsync(omdbApiUrl);
+                var result = JsonConvert.DeserializeObject<Movie>(responseString);
+                if (result.Response == "False")
+                {
+                    throw new Exception();
+                }
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+            
+
+        }
+
+
+        [HttpGet("{Title}/{season}")]
+        public async Task<ActionResult<Ep[]>> SeasonInfo(string title,int season)
+        {
+            try
+            {
+                var t = title;
+                var s = season;
+                var apiKey = _config.GetValue<string>("apiKey");
+                var httpClient = _clientFactory.CreateClient();
+                string omdbApiUrl = $"http://www.omdbapi.com?apikey={apiKey}&t={t}&season={s}";
+                var responseString = await httpClient.GetStringAsync(omdbApiUrl);
+                var result = JsonConvert.DeserializeObject<SeasonInfo>(responseString);
+                if(result.Response == "False")
+                {
+                    throw new Exception();
+                }
+                return Ok(result.Episodes);
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+        
+        }
+    }
+}
---
+++ MoviesInfo.API/SeasonInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesInfo.API
+{
+
+    public class SeasonInfo
+    {
+        public string Title { get; set; }
+        public string Season { get; set; }
+        public string totalSeasons { get; set; }
+        public Ep[] Episodes { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class Ep
+    {
+        public string Title { get; set; }
+        public string Released { get; set; }
+        public string Episode { get; set; }
+        public string imdbRating { get; set; }
+        public string imdbID { get; set; }
+    }
+
+}
